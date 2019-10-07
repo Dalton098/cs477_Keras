d748a919ed6391ae743c842cb2e733a8b94b3fc6
Dalton <dalton098@ymail.com>
2019-10-07 17:31:19 -0400

    Graphing

--- perceptron.py
+++ perceptron.py
@@ -1,11 +1,16 @@
 import numpy as np
 import keras
+import matplotlib.pyplot as plt
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.optimizers import SGD
 from keras.layers import Dense
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Plots the first tensor in the MNIST data
+# plt.imshow(x_train[0], cmap=plt.get_cmap('gray'))
+# plt.show()
 
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
@@ -28,7 +33,7 @@ model.compile(loss = "msle", optimizer = SGD(lr = 0.1),
 metrics=['accuracy'])
 
 batch_size = 32
-epochs = 50
+epochs = 5
 
 
 history = model.fit(x_train, y_train,
@@ -40,3 +45,13 @@ score = model.evaluate(x_test, y_test, verbose=0)
 
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+# Plotting the performance
+history_dict = history.history
+plt.plot(range(epochs), history_dict['loss'], label='Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Performance')
+plt.legend()
+plt.savefig('test.png')
+
+
