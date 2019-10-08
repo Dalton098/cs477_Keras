cdd5be7c1b2d8a8192c0cb12daacccee0befb08d
Dalton <dalton098@ymail.com>
2019-10-08 16:27:06 -0400

    cont

--- perceptron.py
+++ perceptron.py
@@ -8,10 +8,6 @@ from keras.layers import Dense
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Plots the first tensor in the MNIST data
-plt.imshow(x_train[0], cmap=plt.get_cmap('gray'))
-plt.show()
-
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
 x_train = x_train.astype('float32')
@@ -35,7 +31,6 @@ metrics=['accuracy'])
 batch_size = 32
 epochs = 50
 
-
 history = model.fit(x_train, y_train,
                 batch_size=batch_size,
                 epochs=epochs,
@@ -45,11 +40,3 @@ score = model.evaluate(x_test, y_test, verbose=0)
 
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-
-# Plotting the performance
-history_dict = history.history
-plt.plot(range(epochs), history_dict['loss'], label='Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Performance')
-plt.legend()
-plt.savefig('SGD.png')
